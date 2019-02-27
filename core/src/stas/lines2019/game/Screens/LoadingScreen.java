7d2f45a861c48b3b7905189e6d692ba2ab8c121c
Stas <seeyou85@gmail.com>
2019-02-27 20:49:21 +0300

    adding load assets

--- core/src/stas/lines2019/game/Screens/LoadingScreen.java
+++ core/src/stas/lines2019/game/Screens/LoadingScreen.java
@@ -91,7 +91,7 @@ public class LoadingScreen implements Screen {
 	public void render(float delta) {
 		elapsed += Gdx.graphics.getDeltaTime();
 		startTime = TimeUtils.millis();
-		Gdx.gl.glClearColor(0,0,0,1);
+		Gdx.gl.glClearColor(231,223,221,1);
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	    
